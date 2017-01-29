dc8533a97e485a82fb26977eeb09d449c9c0191c
sean_codes <sean.mann@alpinehomeair.com>
2017-01-29 15:05:44 -0600

    [update] - added answers page with questions

--- js/javascript.js
+++ js/javascript.js
@@ -10,10 +10,30 @@ navFix = function(){
     var distanceFromTop = bannerHeight - scrollY;
 
     if(distanceFromTop < 20){
-        nav.style.top = ((0 - distanceFromTop) - 300) + 'px';
+        nav.style.top = ((0 - distanceFromTop) - 1000) + 'px';
         if(!nav.classList.contains('fixedHeader'))
             nav.classList.add('fixedHeader')
     } else {
         nav.classList.remove('fixedHeader')
+    }
+}
+
+search = function(){
+    var searchText = document.getElementById('search').value.toLowerCase();
+    var questions = document.getElementsByClassName('question');
+
+    for(var i = 0; i < questions.length; i++){
+        var question = questions[i];
+        var questionText = question
+            .getElementsByClassName('questionText')[0]
+            .innerHTML.toLowerCase();
+        var questionAnswer = question
+            .getElementsByClassName('questionAnswer')[0]
+            .innerHTML.toLowerCase();
+
+        question.style.display = '';
+        if(!questionText.includes(searchText) && !questionAnswer.includes(searchText)){
+            question.style.display = 'none';
+        }
     }
 }
