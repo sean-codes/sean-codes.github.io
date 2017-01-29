994957eab2ff67f4c8887a39f404494be5bda0a6
sean_codes <sean.mann@alpinehomeair.com>
2017-01-29 16:37:31 -0600

    [update] - instagram icon new page setup

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,9 @@
 window.onscroll = function(){ navFix() }
-
 navFix = function(){
     var nav = document.getElementsByTagName('nav')[0];
     var banner = document.getElementById('banner');
-
     var scrollY = (window.pageYOffset || document.scrollTop)  - (document.clientTop || 0);
     var bannerHeight = banner.offsetHeight;
-
     var distanceFromTop = bannerHeight - scrollY;
 
     if(distanceFromTop < 20){
@@ -36,4 +33,21 @@ search = function(){
             question.style.display = 'none';
         }
     }
+}
+
+contact = function(){
+    var from = document.getElementById("email").value;
+    var reason = document.getElementById("reason").value;
+    var message = document.getElementById("message").value;
+    var params = 'from='+from+'&reason='+reason+'&message='+message;
+
+    var http = new XMLHttpRequest();
+    var url = "https://formspree.io/sean_codes@outlook.com";
+
+    http.open("POST", url, true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.send(params);
+
+    document.getElementById('formContact').style.display = "none";
+    document.getElementById('formSent').style.display = "block";
 }
