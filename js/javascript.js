d72bf1770d93c6dcdd6da55998368bff44d69188
sean_codes <sean.mann@alpinehomeair.com>
2017-01-29 17:21:07 -0600

    [update] - analytics

--- js/javascript.js
+++ js/javascript.js
@@ -51,3 +51,13 @@ contact = function(){
     document.getElementById('formContact').style.display = "none";
     document.getElementById('formSent').style.display = "block";
 }
+
+
+//GOOGLE ANALYTICS TY!
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-91047274-1', 'auto');
+ga('send', 'pageview');
