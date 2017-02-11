6af85406e1b6510d7d3d58a557951649154682a5
sean_codes <sean.mann@alpinehomeair.com>
2017-02-11 02:51:11 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,3 @@
-window.onscroll = function(){ navFix() }
-navFix = function(){
-    var nav = document.getElementsByTagName('nav')[0];
-    var banner = document.getElementById('banner');
-    var scrollY = (window.pageYOffset || document.scrollTop)  - (document.clientTop || 0);
-    var bannerHeight = banner.offsetHeight;
-    var distanceFromTop = bannerHeight - scrollY;
-
-    if(distanceFromTop < 20){
-        nav.style.top = ((0 - distanceFromTop) - 1000) + 'px';
-        if(!nav.classList.contains('fixedHeader'))
-            nav.classList.add('fixedHeader')
-    } else {
-        nav.classList.remove('fixedHeader')
-    }
-}
-
 search = function(){
     var searchText = document.getElementById('search').value.toLowerCase();
     var questions = document.getElementsByClassName('question');
